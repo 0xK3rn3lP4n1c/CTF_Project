ce2b178c1df4ea979dc533a0f2e15d71e58b8899
K3rnel_P4n1c <53450448+Kazuto-Kawahara@users.noreply.github.com>
2023-03-04 20:30:53 +0300

    Authorization is Updated
    Updated the authorization

--- Broken-Access-Control-Admin-Panel/dist/js/authorization.js
+++ Broken-Access-Control-Admin-Panel/dist/js/authorization.js
@@ -1,27 +1,33 @@
-var allowedPages = [
-    "index.html",
-    "pages/user.html"
-  ];
-  
-  window.onload = function() {
+var allowedPages = ["index.html", "pages/user.html"];
+
+window.onload = function() {
     // Mevcut URL'yi alın
-    var currentUrl = window.location.pathname;
-  
-    // Eğer mevcut URL izin verilen sayfaların hiçbirine eşleşmiyorsa ve kullanıcı admin değilse
-    if(!isAllowedPage(currentUrl) && !isAdminUser()) {
-      // Hata mesajı gösterin ve ana sayfaya yönlendirin
-      alert("Bu sayfaya erişim izniniz yok!");
-      window.location.href = "/index.html";
+    var currentUrl = window.location.href;
+    
+    // Eğer mevcut URL izin verilen sayfaların hiçbirine eşleşmiyorsa
+    if (!isAllowedPage(currentUrl)) {
+        // Hata mesajı gösterin ve ana sayfaya yönlendirin
+        alert("Bu sayfaya erişim izniniz yok!");
+        
+        // Eğer sayfa "/pages/" altındaysa
+        if (currentUrl.includes("/pages/")) {
+            // Kök dizinine yönlendirin
+            window.location.href = "../../../index.html";
+        }
+        
+        // Ana sayfaya yönlendirin
+        window.location.href = "../../index.html";
     }
-  };
-  
-  function isAllowedPage(url) {
-    // izin verilen sayfalar dizisinde mevcut URL'yi arayın
-    for(var i=0; i<allowedPages.length; i++) {
-      if(url === allowedPages[i]) {
-        return true;
-      }
+};
+
+function isAllowedPage(url) {
+  // izin verilen sayfalar dizisinde mevcut URL'yi arayın
+  for (var i=0; i<allowedPages.length; i++) {
+    if (url.indexOf(allowedPages[i]) !== -1) {
+      return true;
     }
-    return false;
   }
-  
+  return false;
+}
+
+
